b91a98c22bee1119806fc132bce8553f4c2f04d3
greeshma786 <111418492+greeshma786@users.noreply.github.com>
2022-08-17 21:47:03 +0530

    Add files via upload

---
+++ csharp_parameters/csharpassessment3/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpassessment3
+{
+    class Class2
+    {
+        public static void SUM(params int[] inp)
+        {
+            int res = 0;
+            for(int i = 0; i < inp.Length; i++)
+            {
+                res += inp[i];
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
---
+++ csharp_parameters/csharpassessment3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpassessment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            Console.WriteLine("enter monthly payment");//100
+            long mc = Int64.Parse(Console.ReadLine());
+            obj.Solution(mc);
+            Class2 add = new Class2();
+            Class2.SUM(10, 20, 30);//calculating sum for 3 parameters
+            Class2.SUM(1, 2, 3, 4, 5, 6, 7, 8);//calculating for 8 parameters
+            Console.ReadLine();
+        }
+    }
+}
