4a428eddb573d4cc75fa38bdde6807751e20ec7a
greeshma786 <111418492+greeshma786@users.noreply.github.com>
2022-08-17 21:33:44 +0530

    Add files via upload

---
+++ csharpmatrix/csharpassignment2/Class2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpassignment2
+{
+    class Class2
+    {
+        public static void Solution()
+        {
+            Console.WriteLine("cfvgbhjnnbu");
+            int[,] a = new int[3,3];
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    a[i,j]=Int32.Parse(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(a[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
